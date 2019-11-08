a5cc98f2d9c8ef5ad39758bf46c062119b425213
SmirnovOleg <sm-oleg1209@yandex.ru>
2019-11-08 18:35:02 +0300

    Global refactoring

---
+++ Task04/HtmlReceiver.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    public static class HtmlUtils
+    {
+        public static string GetData(String url)
+        {
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            using HttpWebResponse response = request?.GetResponse() as HttpWebResponse;
+            Stream receiver = response?.GetResponseStream();
+
+            if (response != null && receiver != null && response.StatusCode == HttpStatusCode.OK)
+            {
+                using StreamReader reader = new StreamReader(receiver, Encoding.GetEncoding(response.CharacterSet));
+                return reader.ReadToEnd();
+            }
+            throw new WebException();
+        }
+        
+        public static async Task TraverseUrlsAsync(List<String> urls)
+        {
+            try
+            {
+                await Task.WhenAll(urls.Select(ProcessUrlAsync));
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("Troubles with response from one of urls or status code is not 200");
+            }
+        }
+
+        private static Task ProcessUrlAsync(String url)
+        {
+            return Task.Run(() => Console.WriteLine($"{url} -- {GetData(url).Length}"));
+        }
+    }
+}
--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,67 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Task04
 {
-    static class Program
+    class Program
     {
-        public static async Task Main()
+        public static void Main()
         {
             string url = "https://github.com/EgorkaKulikov/spbu-se2019-autumn/pulls";
             try
             {
-                string data = GetDataFromUrl(url);
+                string data = HtmlUtils.GetData(url);
                 var matches = Regex.Matches(data, @"<a.*href=""(http|https)://(\S*)""")
                     .Select(match => match.Value)
                     .Select(tag =>
                     {
-                        var startIndex = tag.IndexOf("href=\"", StringComparison.Ordinal) + "href=\"".Length;
-                        return tag.Substring(startIndex,tag.Length - startIndex - 1);
+                        string prefix = "href=\"";
+                        var startIndex = tag.IndexOf(prefix, StringComparison.Ordinal) + prefix.Length;
+                        var length = tag.Length - startIndex - 1;
+                        return tag.Substring(startIndex, length);
                     })
                     .ToList();
-                await ProcessUrls(matches);
+                HtmlUtils.TraverseUrlsAsync(matches).GetAwaiter().GetResult();
             }
             catch (WebException)
             {
                 Console.WriteLine($"Troubles with response from {url} or status code is not 200");
             }
-        }
-
-        public static async Task ProcessUrls(List<String> urls)
-        {
-            try
-            {
-                await Task.WhenAll(urls.Select(ProcessUrlAsync));
-            }
-            catch (WebException)
-            {
-                Console.WriteLine("Troubles with response from one of urls or status code is not 200");
-            }
-        }
-
-        private static Task ProcessUrlAsync(String url)
-        {
-            return Task.Run(() => Console.WriteLine($"{url} -- {GetDataFromUrl(url).Length}"));
-        }
-
-        private static string GetDataFromUrl(String url)
-        {
-            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-            using HttpWebResponse response = request?.GetResponse() as HttpWebResponse;
-            Stream receiver = response?.GetResponseStream();
-
-            if (response != null && receiver != null && response.StatusCode == HttpStatusCode.OK)
-            {
-                using StreamReader reader = new StreamReader(receiver, Encoding.GetEncoding(response.CharacterSet));
-                return reader.ReadToEnd();
-            }
-            throw new WebException();
         }
     }
 }
